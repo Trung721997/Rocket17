0ef63572444fd4cba3c482cf8094f3a6577e7efc
Trung721997 <trungnv97.haui@gmail.com>
2021-07-31 11:16:16 +0700

    MySQL

---
+++ MySQL Testing and extra 1/Testing_System_Assignment_1.sql
@@ -0,0 +1,110 @@
+DROP DATABASE IF EXISTS 	Testing_System_Assignment_1 ;
+CREATE DATABASE 			Testing_System_Assignment_1 ;
+USE							Testing_System_Assignment_1 ;
+
+-- Tạo bẳng Department
+DROP DATABASE IF EXISTS	Department;
+CREATE TABLE			Department(
+DepartmentID			TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+DepartmentName			NVARCHAR(20) NOT NULL
+);
+
+-- Tạo bảng Position
+DROP DATABASE IF EXISTS `Position`;
+CREATE TABLE 			`Position`(
+PositionID				TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+PositionName			ENUM('Dev', 'Test', 'Scrum Master', 'PM') NOT NULL
+);
+
+-- Tạo bảng Account
+DROP DATABASE IF EXISTS `Account`;
+CREATE TABLE 			`Account`(
+AccountID				INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+Email					NVARCHAR(50) UNIQUE NOT NULL,
+Username				NVARCHAR(20) UNIQUE NOT NULL CHECK( length(Username) > 6),
+FullName				NVARCHAR(20) NOT NULL,
+DepartmentID			TINYINT UNSIGNED NOT NULL,
+PositionID				TINYINT UNSIGNED NOT NULL,
+CreateDate				DATE NOT NULL,
+FOREIGN KEY (DepartmentID) REFERENCES Department(DepartmentID),
+FOREIGN KEY (PositionID) REFERENCES `Position`(PositionID) 
+);
+
+-- Tạo bảng Group
+DROP DATABASE IF EXISTS `Group`;
+CREATE TABLE 			`Group`(
+GroupID					TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+GroupName				NVARCHAR(20) UNIQUE NOT NULL,
+CreatorID				TINYINT UNSIGNED NOT NULL,
+CreateDate				DATE NOT NULL
+);
+
+-- Tạo bảng GroupAccount
+DROP DATABASE IF EXISTS GroupAccount;
+CREATE TABLE			GroupAccount(
+GroupID					TINYINT UNSIGNED NOT NULL,
+AccountID				INT UNSIGNED NOT NULL,
+JoinDate				DATE NOT NULL,
+PRIMARY KEY	(GroupID, AccountID),
+FOREIGN KEY	(GroupID) REFERENCES `Group`(GroupID),
+FOREIGN KEY	(AccountID) REFERENCES `Account`(AccountID)
+);
+
+-- Tạo bảng TypeQuestion
+DROP DATABASE IF EXISTS TypeQuestion;
+CREATE TABLE			TypeQuestion (
+TypeID					TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+TypeName				ENUM('Essay', 'Multiple Choice' ) NOT NULL
+);
+
+-- Tạo bảng CategoryQuestion
+DROP DATABASE IF EXISTS CategoryQuestion;
+CREATE TABLE			CategoryQuestion(
+CategoryID				TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+CategoryName			ENUM('Java', '.NET', 'SQL', 'Postman', 'Ruby') NOT NULL
+);
+
+-- Tạo bảng Question
+DROP DATABASE IF EXISTS Question;
+CREATE TABLE			Question(
+QuestionID				INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+Content					NVARCHAR(100) UNIQUE NOT NULL,
+CategoryID				TINYINT UNSIGNED NOT NULL,
+TypeID					TINYINT UNSIGNED NOT NULL,
+CreatorID				INT UNSIGNED NOT NULL,
+CreateDate				DATE NOT NULL,
+FOREIGN KEY (CategoryID) REFERENCES CategoryQuestion(CategoryID),
+FOREIGN KEY (TypeID) REFERENCES TypeQuestion(TypeID),
+FOREIGN KEY (CreatorID) REFERENCES	`Account` (AccountID)
+);
+
+-- Tạo bảng Answer
+DROP DATABASE IF EXISTS Answer;
+CREATE TABLE 			Answer(
+AnswerID				INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+Content					NVARCHAR(100) UNIQUE NOT NULL,
+QuestionID				INT UNSIGNED NOT NULL,
+isCorrect				ENUM ('Đúng', 'Sai') NOT NULL,
+FOREIGN KEY (QuestionID) REFERENCES Question(QuestionID)
+);
+
+-- Tạo bảng Exam
+DROP DATABASE IF EXISTS Exam;
+CREATE TABLE			Exam(
+ExamID					TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+`Code`					TINYINT UNSIGNED UNIQUE NOT NULL,
+Title					NVARCHAR(50) UNIQUE NOT NULL,
+CategoryID				TINYINT UNSIGNED UNIQUE NOT NULL,
+Duration				TINYINT UNSIGNED NOT NULL,
+CreatorID				TINYINT UNSIGNED NOT NULL,
+CreateDate				DATE NOT NULL
+);
+
+-- Tạo bảng ExamQuestion
+DROP DATABASE IF EXISTS ExamQuestion;
+CREATE TABLE			ExamQuestion(
+ExamID					TINYINT UNSIGNED NOT NULL,
+QuestionID				INT UNSIGNED NOT NULL,
+FOREIGN KEY (ExamID) REFERENCES Exam(ExamID),
+FOREIGN KEY (QuestionID) REFERENCES Question(QuestionID)
+);
